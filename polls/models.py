1a91f30c9b0c5d1d254fbc6631beeb585b18779d
Daniel Lowe <dnllowe@yahoo.com>
2017-05-10 21:04:43 -0400

    feat(choices): choices implemented This was not committed last time

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# Questions for Poll
 class Question(models.Model):
-  question = models.CharField(max_length = 200)
+  text = models.CharField(max_length = 200)
   date_published = models.DateTimeField()
+
+# Choices for Poll
+class Choice(models.Model):
+  question = models.ForeignKeyField(Question, on_delete=models.CASCADE)
+  text = models.CharField(max_length = 200)
+  votes = models.IntegerField(default = 0)
