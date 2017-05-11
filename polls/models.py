29fb3e187b21a82661dff838d1f26c2cc97e1fa8
Daniel Lowe <dnllowe@yahoo.com>
2017-05-10 22:39:11 -0400

    style(models): added documentation

--- polls/models.py
+++ polls/models.py
@@ -4,11 +4,13 @@ from django.db import models
 
 # Questions for Poll
 class Question(models.Model):
+  """ Model for poll questions """
   text = models.CharField(max_length = 200)
   date_published = models.DateTimeField()
 
 # Choices for Poll
 class Choice(models.Model):
+  """ Model for poll choices for each question """
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
   text = models.CharField(max_length = 200)
   votes = models.IntegerField(default = 0)
